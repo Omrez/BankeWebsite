6241ab2432cf79752b787163fed1686d65ee4764
reez <zwitwitwit@gmail.com>
2021-10-19 14:08:47 +0200

    newst

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 import { HomeComponent } from './home/home.component';
+import { AuthService } from './services/auth.service';
+import { TokenStorageService } from './services/token-storage.service';
 
 
 
@@ -20,17 +22,40 @@ import { HomeComponent } from './home/home.component';
 
 
 export class AppComponent implements OnInit {
-
-
   title = 'BankeWebsite';
 
-  
+
+  // register
+  registerForm: any = {
+    fName: null,
+    lName: null,
+    mail: null,
+    pass: null
+  };
+
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  // Login
+  loginForm: any = {
+    lMail: null,
+    lPass: null
+  };
+
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errMessage = '';
+  roles: string[] = [];
+
+  name?: string;
+
   constructor(
-     private ptoService: PtoService,
-     private problemService: ProblemService,
      public _router: Router,
      public _location: Location,
-     public translate: TranslateService
+     public translate: TranslateService,
+     private authService: AuthService,
+     private tokenStorage: TokenStorageService
      ) {
        translate.addLangs(['EN','DK']);
        translate.setDefaultLang('EN');
@@ -43,13 +68,67 @@ export class AppComponent implements OnInit {
     
   }
      
-  ngOnInit(){ 
-   
+  ngOnInit(): void{ 
+    const user = this.tokenStorage.getUser();
+    this.isLoggedIn = !!this.tokenStorage.getToken();
+
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getUser().roles;
+    }
+
+
+    this.name = user.name;
+  }
+
+  submitRegister(): void{
+    const {fName, lName, mail, pass} = this.registerForm;
+
+    this.authService.register(fName, lName, mail, pass).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
         
+      }
+    );
+  } //submit Register end
+
+  submitLogin(): void{
+    const {lMail, lPass} = this.loginForm;
+
+    this.authService.login(lMail, lPass).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenStorage.getUser().roles;
+        this.reloadPage();
+        
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    );
+  } //submit Login end
   
+  reloadPage(): void {
+    window.location.reload();
+  }
+
+  logout(): void {
+    this.tokenStorage.signOut();
+    window.location.reload();
   }
 
   
-}
+} //class appcomponent end
 
 
--- src/assets/js/jsFunc.js
+++ src/assets/js/jsFunc.js
@@ -22,15 +22,28 @@ function myFunc() {
 }
 
 function loginForm(){
-    document.getElementById('loginContent').style.display = 'block';
+    var popup = document.getElementById('loginContent').style.display = 'block';
     
+    //var popup = document.getElementById("loginContent");
+    popup.classList.toggle("show");
     //document.body.style.overflow = 'hidden';
+}
+
+function signForm(){
+    document.getElementById('signupContent').style.display = 'none';
+    document.getElementById('loginContent').style.display = 'block';
+}
+
+function signupForm(){
+    document.getElementById('loginContent').style.display = 'none';
+    document.getElementById('signupContent').style.display = 'block';
 }
 
 function hidePopup(){
     document.querySelector(".modal").style.display = "none";
-   
+    document.querySelector(".modal2").style.display = "none";  
 }
+
 
 function getIdString(){
     var select = document.getElementById('problemstillinger');
